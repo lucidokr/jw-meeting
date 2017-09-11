e9183f42f83f228146ecdbb8749a7e1476ce5536
Lucido Kristian <Kristian.Lucido@nttdata.com>
2017-09-11 10:03:28 +0200

    change URL API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  url: "https://jw-meeting.azurewebsites.net/api",
-  MONGO_URI: "mongodb://jw-meeting:vpfchGCXuIRDNGUqVq7xrmiExVNUk3pmty8PpSkbGjgYjmV7laEohmAT7g2teN6bIZc0Twp6Mqre6VUs1u9wcg==@jw-meeting.documents.azure.com:10255/jw-meeting?ssl=true",
+  url: "https://jw-meeting.herokuapp.com/api"
 };
