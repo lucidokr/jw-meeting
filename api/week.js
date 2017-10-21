a1833dbb0703c1f5f8a5c8d96e36e04cefc6161a
Lucido Kristian <Kristian.Lucido@nttdata.com>
2017-10-21 15:50:20 +0200

    send week mail

--- api/week.js
+++ api/week.js
@@ -474,20 +474,23 @@ router.route('/:week_id')
             var week = week[0];
             if(process.env.SEND_ASSEGNATION == "true"){
               var brother = week.bibleReading.primarySchool.student;
-              brother.email = "lucido.kristian@gmail.com";
-              mailAssegnationToSend.push({mail:brother.email, brother: brother.surname+ ' '+brother.name, assistant:'', type:"bibleReading", school:brother.student.lastSchool, date:week.date, point:brother.student.bibleReadingPendingStudyNumber})
+              // brother.email = "lucido.kristian@gmail.com";
+              if(brother.email){
+                mailAssegnationToSend.push({mail:brother.email, brother: brother.surname+ ' '+brother.name, assistant:'', type:"bibleReading", school:brother.student.lastSchool, date:week.date, point:brother.student.bibleReadingPendingStudyNumber})
+              }
               if (!week.presentationExercise.enabled) {
 
                   brother = week.bibleReading.secondarySchool.student;
-                  brother.email = "lucido.kristian@gmail.com";
-                  mailAssegnationToSend.push({mail:brother.email, brother: brother.surname+ ' '+brother.name, assistant:'', type:"bibleReading", school:brother.student.lastSchool, date:week.date, point:brother.student.bibleReadingPendingStudyNumber})
-
+                  // brother.email = "lucido.kristian@gmail.com";
+                  if(brother.email){
+                    mailAssegnationToSend.push({mail:brother.email, brother: brother.surname+ ' '+brother.name, assistant:'', type:"bibleReading", school:brother.student.lastSchool, date:week.date, point:brother.student.bibleReadingPendingStudyNumber})
+                  }
                   var arr = ["initialCall", "returnVisit", "bibleStudy"];
                   var schools = ["primarySchool", "secondarySchool"];
                   arr.forEach(function (partType) {
                     schools.forEach(function (school) {
                       var brother = week[partType][school].student;
-                      brother.email = "lucido.kristian@gmail.com";
+                      // brother.email = "lucido.kristian@gmail.com";
                         if (brother.email && process.env.SEND_ASSEGNATION == "true") {
                           var obj = {
                             mail: brother.email,
