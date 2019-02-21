afd35b11448e05a37de5112e06edb4e116eeb6a8
Lucido Kristian <kristian.lucido@nttdata.com>
2019-02-21 10:21:20 +0100

    dump PROD + job send reminder

---
+++ send-reminder.js
@@ -0,0 +1,100 @@
+
+var Week = require('./api/models/weekMeeting');
+const MONTH_NAMES = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+  "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+];
+const DAY_NAMES = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
+var MAIL = require('./api/mail/send-mailgun');
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
+    var config = require('./api/env.json')['development'];
+    process.env.MONGO_DB_URI = config.MONGO_DB_URI;
+    process.env.SECRET = config.SECRET;
+    process.env.GMAIL_ACCOUNT = config.GMAIL_ACCOUNT;
+    process.env.GMAIL_ACCOUNT_PASSWORD = config.GMAIL_ACCOUNT_PASSWORD;
+    process.env.SEND_ASSEGNATION = config.SEND_ASSEGNATION;
+}
+
+// JOB FOR SEND ASSEGNATIONS
+
+  console.log('Start reminder assegnations job');
+
+  if(new Date().getDay() == 1 && process.env.SEND_ASSEGNATION == "true"){
+      try{
+        var cutoff = new Date();
+        cutoff.setDate(cutoff.getDate()-6);
+        var weeks = await Week
+        .find({
+          date:{
+            $gt: cutoff,
+            $lt: new Date()
+          }
+        })
+          .sort([
+              ['date', 'descending']
+          ])
+          .populate({ path: 'bibleReading.primarySchool.student', populate: { path: 'student'} })
+          .populate({ path: 'bibleReading.secondarySchool.student', populate: { path: 'student'} })
+          .populate({ path: 'ministryPart.primarySchool.student', populate: { path: 'student'} })
+          .populate({ path: 'ministryPart.secondarySchool.student', populate: { path: 'student'} })
+          .populate('ministryPart.secondarySchool.assistant')
+          .populate('ministryPart.primarySchool.assistant')
+      }catch(e){
+        console.log("Error on find weeks", e)
+        return;
+      }
+      console.log("Weeks found:", weeks.length)
+
+      var schools = ["primarySchool"];
+      var mailAssegnationReminderToSend = [];
+      for (let week of weeks){
+        var date = new Date(week.date);
+        var month = MONTH_NAMES[date.getMonth()];
+        var day = DAY_NAMES[date.getDay()-1];
+        var strDate = day+" " +date.getDate() + " " + month + " " + date.getFullYear();
+
+        schools = ["primarySchool"];
+        if(week.secondarySchool){
+          schools.push("secondarySchool");
+        }
+          if (week.type.meeting && !week.supervisor) {
+            for(let school of schools){
+              var brother = week.bibleReading[school].student;
+              if (brother.email && process.env.SEND_ASSEGNATION == "true") {
+                  console.log("Reminder to send:", brother.name + ' ' + brother.surname)
+                  mailAssegnationReminderToSend.push({
+                    mail: brother.email,
+                    brother: brother.name + ' ' + brother.surname,
+                    assistant: "",
+                    type: week.bibleReading.label,
+                    school: brother.student.lastSchool,
+                    date: strDate
+                  });
+              }
+              for(let part of week.ministryPart){
+                if(part.forStudent){
+                  var brother = part[school].student;
+                  if (brother.email && process.env.SEND_ASSEGNATION == "true") {
+                      console.log("Reminder to send:", brother.name + ' ' + brother.surname)
+                      mailAssegnationReminderToSend.push({
+                        mail: brother.email,
+                        brother: brother.name + ' ' + brother.surname,
+                        assistant: (part[school].assistant ? '<h3>Assistente: '+part[school].assistant.surname + ' ' + part[school].assistant.name+'</h3>' : null),
+                        type: part.html,
+                        school: brother.student.lastSchool,
+                        date: strDate
+                      });
+                  }
+                }
+              }
+            }
+            console.log("Mail reminder to send:", mailAssegnationReminderToSend.length)
+            if(mailAssegnationReminderToSend.length > 0){
+              //MAIL.sendReminderAssegnations(mailAssegnationReminderToSend);
+            }
+          }
+      }
+      console.log('Finish reminder assegnations job');
+      console.log('---------------------------------');
+
+}
