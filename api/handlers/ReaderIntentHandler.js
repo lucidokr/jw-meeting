4842252ef67f97249a09f5bd5da878c015ea46ee
Kristian Lucido <kristian.lucido@nttdata.com>
2019-11-13 14:18:10 +0100

    fix intent name

--- api/handlers/ReaderIntentHandler.js
+++ api/handlers/ReaderIntentHandler.js
@@ -4,7 +4,7 @@ const Week = require('../models/weekMeeting');
 module.exports = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'PresidentIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'ReaderIntent';
   },
   async handle(handlerInput) {
     try{
