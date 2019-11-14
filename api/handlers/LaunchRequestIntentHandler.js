2bc91600e21811bdae0997fd63dc5e5ffb9280d7
Kristian Lucido <kristian.lucido@nttdata.com>
2019-11-14 11:39:16 +0100

    launch request

--- api/handlers/LaunchRequestIntentHandler.js
+++ api/handlers/LaunchRequestIntentHandler.js
@@ -3,12 +3,12 @@ module.exports = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Benvenuto nel programma adunanza Vita Cristiana e Ministero!';
+    const speechText = 'Benvenuto nel programma adunanza Vita Cristiana e Ministero! Puoi chiedermi l\'oratore di una parte dell\'adunanza di questa settimana. Quale parte ti interessa?';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Benvenuto', speechText)
+      .withSimpleCard('Programma adunanza Vita Cristiana e Ministero', speechText)
       .getResponse();
   }
 }
